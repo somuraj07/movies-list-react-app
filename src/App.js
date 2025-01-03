17bdff657515b8f715688df115fc9d47fe299961
somuraj07 <somasankar33@gmail.com>
2025-01-03 19:52:08 +0530

    C

--- src/App.js
+++ src/App.js
@@ -1,62 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import MovieCard from './MovieCard';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import SearchIcon from './SearchIcon'; 
+import './App.css';
+import SearchIcon from './search.svg'
 
  const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=6f042053";
 
-function App() {
+ const App = () => {
 
-  const [movies,setMovies] = useState([]);
-  const [searchTerm,setSearchTerm] = useState("");
-
+  const [searchTerm,setSearchTerm] = useState('');
+  const [movie,setMovies] = useState([]);
+  
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies('Avengers');
   }, []);
-  const searchMovies = async(title) => 
-   {
-    try {
-      const response = await fetch(`<span class="math-inline">\{API\_URL\}&s\=</span>{title}`);
-      const data = await response.json();
-      if (!response.ok) {
-        throw new Error('Failed to fetch movies');
+  
+  
+  const searchMovies = async (title) => {
+      const response  = await fetch(`${API_URL}&s=${title}`);
+       const res = await response.json();
+      if (res.Search) {
+        setMovies(res.Search);
       }
-      setMovies(data.Search || []);
-    } catch (error) {
-      console.error('Error fetching movies:', error);
+      console.log(res);
     }
-  };
-
-     return (
-     <div>
-      <div className='app'>
-      <h1>MOVIELAND</h1>
-   </div>
-   <div className='search'>
-    <input 
-    value={searchTerm}
-    onChange={(e) => setSearchTerm(e.target.value)}
-    placeholder='seach for movies'
-    />
-    <img 
-    src={SearchIcon}
-    alt='search'
-    onClick={() => searchMovies(searchTerm)} />
-
-  {movies.length > 0 && (
-  <div className='container'>
-    {movies.map(movie) =>(<MovieCard movie={movie} key={movie.imdbID} />)}
+ 
+      return (
+  <div> 
+      <h1>PURPLE</h1>
+      <div className='search'>
+          <input 
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder='Search for a movie'
+          />
+          <img 
+          src={SearchIcon}
+          alt='search'
+          onClick={() => searchMovies(searchTerm)}
+          />
+      </div>
+      {movie?.length > 0 ? (
+             <div className="container">
+              {movie.map((movie) => (
+                  <MovieCard movie={movie} />
+              ))}
+              </div>
+          ) : (
+              <div className="empty">
+                  <h1>NO MOVIES FOUND</h1>
+              </div>
+          )}
+  
   </div>
-)}
-
-{!movies.length && ( 
-  <div className='empty'>
-    <h2>no movies found</h2>
-  </div>
-)}
-   </div>
-     </div>
-   
-  )};
-
+         
+  
+  )
+  }
+  
 export default App;
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 
-export const MovieCard = ({movie : {imdbID ,Year,Poster,Title,Type}}) => {
+ const MovieCard = ({movie:{ imdbID, Year ,Poster, Title, Type}}) => {
   return (
-   <div className='movie' key={imdbID}>
-<div>
-    <p>{Year}</p>
-</div>
-<div>
-    <img src={Poster !== "N/A" ? Poster: "https://via.placeholder.com/400"} alt={Title} />
-</div>
-<div>
-    <span>{Type}</span>
-    <h3>{Title}</h3>
-</div>
-   </div>
+    <div className='movie' key={imdbID}>
+        <div>
+            <p>{Year}</p>
+        </div>
+        <div>
+            <img src={Poster !== 'N/A' ? Poster : 'https://via.placeholder.com/300x450'} alt={Title}/>
+        </div>
+        <div>
+            <span>
+                {Type}
+            </span>
+            <h3>
+                {Title}
+            </h3>
+        </div>
+    </div>
   )
 }
-
 export default MovieCard;
